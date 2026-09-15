4507a5f8c91827ccdab543676dfbe0c667bd03be
Rizqi Arya Kuskhilbyano <108065333+Kenrzq@users.noreply.github.com>
2026-09-15 20:22:40 +0700

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -111,7 +111,7 @@ void run_kruskal(int skip_node) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <graph.txt> [--fail-edge u v | --fail-node n]\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input.txt> [--fail-edge u v | --fail-node n]\n", argv[0]);
         return 1;
     }
 
